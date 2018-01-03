df96d90b4fed9815be6cbf283f0699d79bb6e928
rabbits-2 <rabbits_2@126.com>
2018-01-03 22:24:01 +0800

    每日更新一下

--- python/2-15.py
+++ python/2-15.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 #coding:utf-8
 
-#用户输入文件名，然后打开文件，并显示他的内容到屏幕上，
+#如何打开文件
 
+#用户输入文件名，然后打开文件，并显示他的内容到屏幕上，
 filename = raw_input('Enter your name: ')
 fobj = open(filename,'r')
 for eachLine in fobj:
---
+++ python/2-21-1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+1 + 2*4
+
+print 1 + 2*4
+
+aL = raw_input('Enter zi fu:')
+
+print aL
+
+num = raw_input('Enter a number:')
+
+print num
+
+print int(num)
+
